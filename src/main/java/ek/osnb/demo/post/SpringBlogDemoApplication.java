db672c10c5778d7254f537d4473d767076310c32
osman-butt <butt.osman@gmail.com>
2026-09-10 09:08:46 +0200

    Init commit

---
+++ src/main/java/ek/osnb/demo/post/SpringBlogDemoApplication.java
@@ -0,0 +1,13 @@
+package ek.osnb.demo.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ek/osnb/demo/post/SpringBlogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ek.osnb.demo.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
